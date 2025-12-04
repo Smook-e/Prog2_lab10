4d4bf651674bf636bcf1c06480dd67966fe9326a
EngYoussefSharaf <sharafyoussef693@gmail.com>
2025-12-04 16:13:26 +0200

    update filemanager

--- storageManager/FileManager.java
+++ storageManager/FileManager.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package storageManager;
-
-/**
- *
- * @author Mega Store
- */
+import java.io.*;
 public class FileManager {
-    
+    public static void saveBoard(String folder, int[][] board) throws IOException {
+        new File(folder).mkdirs();    
+        BufferedWriter writer = new BufferedWriter(new FileWriter(folder + "/game.txt"));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                writer.write(board[i][j] + " ");
+            }
+            writer.newLine();
+        }
+        writer.close();
+    }
+    public static int[][] loadBoard(String folder) throws IOException {
+        int[][] board = new int[9][9];
+        
+        BufferedReader reader = new BufferedReader(new FileReader(folder + "/game.txt"));
+        for (int i = 0; i < 9; i++) {
+            String[] values = reader.readLine().trim().split("\\s+");
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(values[j]);
+            }
+        }
+        reader.close();
+        
+        return board;
+    }
+    public static boolean exists(String folder) {
+        return new File(folder + "/game.txt").exists();
+    }
+    public static void delete(String folder) {
+        new File(folder + "/game.txt").delete();
+    }
 }
