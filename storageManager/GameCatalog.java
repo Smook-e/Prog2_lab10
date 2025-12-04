689cc6c251a1b8ccc1dd5ecd732ac9ac4af13595
EngYoussefSharaf <sharafyoussef693@gmail.com>
2025-12-04 18:27:26 +0200

    Update GameCatalog.java

--- storageManager/GameCatalog.java
+++ storageManager/GameCatalog.java
@@ -1,4 +1,21 @@
 package storageManager;
+
+import model.DifficultyLevel;
+
 public class GameCatalog {
-    
+    public boolean hasEasy() {
+        return FileManager.exists(DifficultyLevel.EASY.getfolderName());
+    }
+    public boolean hasMedium() {
+        return FileManager.exists(DifficultyLevel.MEDIUM.getfolderName());
+    }
+    public boolean hasHard() {
+        return FileManager.exists(DifficultyLevel.HARD.getfolderName());
+    }
+    public boolean hasIncomplete() {
+        return FileManager.exists("current"); 
+    }
+    public boolean hasAllLevels() {
+        return hasEasy() && hasMedium() && hasHard();
+    }
 }
