f69e231b9298862d1d05700d1235a97fe37ad49b
EngYoussefSharaf <sharafyoussef693@gmail.com>
2025-12-12 00:36:13 +0200

    make game storage manager

--- src/storageManager/FileManager.java
+++ src/storageManager/FileManager.java
@@ -1,20 +1,19 @@
 package storageManager;
 import java.io.*;
 public class FileManager {
-    public static void saveBoard(String folder, int[][] board) throws IOException {
+    public static void saveBoard(String folder, SudokuBoard board) throws IOException {
         new File(folder).mkdirs();    
         BufferedWriter writer = new BufferedWriter(new FileWriter(folder + "/game.txt"));
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                writer.write(board[i][j] + " ");
+                writer.write(SudokuBoard.getGrid()[i][j]+ " ");//getGrid for return int[][]
             }
             writer.newLine();
         }
         writer.close();
     }
-    public static int[][] loadBoard(String folder) throws IOException {
+    public static SudokuBoard loadBoard(String folder) throws IOException {
         int[][] board = new int[9][9];
-        
         BufferedReader reader = new BufferedReader(new FileReader(folder + "/game.txt"));
         for (int i = 0; i < 9; i++) {
             String[] values = reader.readLine().trim().split("\\s+");
@@ -23,7 +22,7 @@ public class FileManager {
             }
         }
         reader.close(); 
-        return board;
+        return new SudokuBoard(board);//constructor for SudokuBoard
     }
     public static boolean exists(String folder) {
         return new File(folder + "/game.txt").exists();
@@ -32,6 +31,10 @@ public class FileManager {
         if(exists(folder))
         {          
             new File(folder + "/game.txt").delete();
+        }
+        else
+        {
+            System.out.println("error deleting game");
         }
     }
 }
--- src/storageManager/GameStorgeManager.java
+++ src/storageManager/GameStorgeManager.java
@@ -1,4 +1,52 @@
 package storageManager;
+
+import java.io.IOException;
+import model.DifficultyLevel;
+
 public class GameStorgeManager {
-    
+
+    public void saveGame(DifficultyLevel level, SudokuBoard board) {
+        try {
+            FileManager.saveBoard(level.getfolderName(), board);
+        } catch (IOException ex) {
+            System.out.println("error saving game");
+        }
+
+    }
+
+    public SudokuBoard loadGame(DifficultyLevel level) {
+        try {
+            return FileManager.loadBoard(level.getfolderName());
+        } catch (IOException ex) {
+            System.out.println("error loading game");
+        }
+    }
+
+    public void deleteGame(DifficultyLevel level) {
+        try {
+            FileManager.delete(level.getfolderName());
+        } catch (IOException ex) {
+            System.out.println("error deleting game");
+        }
+    }
+
+    public void saveCurrentGame(SudokuBoard board) {
+        try {
+            FileManager.saveBoard("current", board);
+        } catch (IOException ex) {
+            System.out.println("error saving game");
+        }
+    }
+
+    public void loadCurrentGame() {
+        try {
+            FileManager.loadBoard("current");
+        } catch (IOException ex) {
+            System.out.println("error loading game");
+        }
+    }
+
+    public void hasCurrentGame() {
+        FileManager.exists("current");
+    }
 }
