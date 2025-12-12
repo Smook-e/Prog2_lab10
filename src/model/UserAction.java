3bd69cadba7f657f2d787e6a1d21a9402c04a937
sara-0011 <sarahabiiba@icloud.com>
2025-12-12 23:37:21 +0200

    commit to sudokuView

--- src/model/UserAction.java
+++ src/model/UserAction.java
@@ -9,5 +9,19 @@ package model;
  * @author HP
  */
 public class UserAction {
-    
-}
+    private int row;
+    private int column;
+    private int num;
+    private int prevNum;
+    public UserAction(int row, int column, int num,int prevNum)
+    {
+        this.row=row;
+        this.column=column;
+        this.num=num;
+        this.prevNum=prevNum;
+    }
+    public int getRow(){return row;}
+    public int getColumn(){return column;}
+    public int getNum(){return num;}
+    public int getPrevNum(){return prevNum;}
+ }
