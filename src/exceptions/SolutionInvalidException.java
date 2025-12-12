cda28fcbdeee3be725065fc1af282e9be87b1e66
sara-0011 <sarahabiiba@icloud.com>
2025-12-12 21:09:53 +0200

    commit to solutionInvalidException

---
+++ src/exceptions/SolutionInvalidException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author HP
+ */
+public class SolutionInvalidException extends Exception {
+     public SolutionInvalidException()
+    {
+        super();
+    }
+    public SolutionInvalidException(String message)
+    {
+        super(message);
+    }
+}
