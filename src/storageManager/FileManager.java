5b1a565387b356cf9323363a9f6bf306d1da40d4
Smook-e <ahmednaser2382005@gmail.com>
2025-12-12 19:00:16 +0200

    reversed FileManager changes

--- src/storageManager/FileManager.java
+++ src/storageManager/FileManager.java
@@ -1,63 +1,36 @@
 package storageManager;
 import java.io.*;
-import java.util.Arrays;
-
-
-import model.SudokuBoard;
-
+import javaapplication19.SudokuBoard;
 public class FileManager {
-    public static void saveBoard(File file, SudokuBoard board) throws IOException {
-
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    bw.write(String.valueOf(board.getGrid(i,j)));
-                    if (j < 8) bw.write(",");
-                }
-                bw.newLine();
+    public static void saveBoard(String folder, SudokuBoard board) throws IOException {
+        new File(folder).mkdirs();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(folder + "/game.txt"));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                writer.write(board.getGrid()[i][j]+ " ");//getGrid for return int[][]
             }
+            writer.newLine();
         }
+        writer.close();
     }
-    public static SudokuBoard loadBoard(File file) throws IOException {
-        int[][] grid = new int[9][9];
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            for (int i = 0; i < 9; i++) {
-                String line = br.readLine();
-                if (line == null) {
-                    throw new IOException("file has less than 9 rows");
-                }
-                String[] values = line.split(",");
-                if (values.length != 9) {
-                    throw new IOException("Row " + (i + 1) + " does not contain 9 numbers");
-                }
-                for (int j = 0; j < 9; j++) {
-                    String val = values[j].trim();
-                    if (val.isEmpty() || val.equals("0")) {
-                        grid[i][j] = 0;
-                    } else {
-                        int num = Integer.parseInt(val);
-                        if (num < 1 || num > 9) {
-                            throw new IOException("Invalid number in grid: " + num + " at row " + (i+1) + ", col " + (j+1));
-                        }
-                        grid[i][j] = num;
-                    }
-                }
+    public static SudokuBoard loadBoard(String folder) throws IOException {
+        int[][] board = new int[9][9];
+        BufferedReader reader = new BufferedReader(new FileReader(folder + "/game.txt"));
+        for (int i = 0; i < 9; i++) {
+            String[] values = reader.readLine().trim().split("\\s+");
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(values[j]);
             }
-            // Check for extra lines
-            if (br.readLine() != null) {
-                System.err.println("Warning: File has more than 9 rows. Extra lines ignored.");
-            }
-        } catch (NumberFormatException e) {
-            throw new IOException("Invalid number format in file: " + e.getMessage());
         }
-        return new SudokuBoard(grid);
+        reader.close();
+        return new SudokuBoard(board);//constructor for SudokuBoard
     }
     public static boolean exists(String folder) {
         return new File(folder + "/game.txt").exists();
     }
     public static void delete(String folder) {
         if(exists(folder))
-        {          
+        {
             new File(folder + "/game.txt").delete();
         }
         else
--- src/storageManager/GameStorageManager.java
+++ src/storageManager/GameStorageManager.java
@@ -1,52 +1,51 @@
-//package storageManager;
-//
-//import java.io.IOException;
-//import model.DifficultyLevel;
-//import model.SudokuBoard;
-//
-//
-//public class GameStorageManager {
-//
-//    public void saveGame(DifficultyLevel level, SudokuBoard board) {
-//        try {
-//            FileManager.saveBoard(level.getfolderName(), board);
-//        } catch (IOException ex) {
-//            System.out.println("error saving game");
-//        }
-//
-//    }
-//
-//    public SudokuBoard loadGame(DifficultyLevel level) {
-//        try {
-//            return FileManager.loadBoard(level.getfolderName());
-//        } catch (IOException ex) {
-//            System.out.println("error loading game");
-//            return null;
-//        }
-//    }
-//
-//    public void deleteGame(DifficultyLevel level) {
-//        FileManager.delete(level.getfolderName());
-//    }
-//
-//    public void saveCurrentGame(SudokuBoard board) {
-//        try {
-//            FileManager.saveBoard("current", board);
-//        } catch (IOException ex) {
-//            System.out.println("error saving game");
-//        }
-//    }
-//
-//    public SudokuBoard loadCurrentGame() {
-//        try {
-//            return FileManager.loadBoard("current");
-//        } catch (IOException ex) {
-//            System.out.println("error loading game");
-//            return null;
-//        }
-//    }
-//
-//    public boolean hasCurrentGame() {
-//        return FileManager.exists("current");
-//    }
-//}
+package storageManager;
+
+import java.io.IOException;
+import model.DifficultyLevel;
+import javaapplication19.SudokuBoard;
+
+public class GameStorageManager {
+
+    public void saveGame(DifficultyLevel level, SudokuBoard board) {
+        try {
+            FileManager.saveBoard(level.getfolderName(), board);
+        } catch (IOException ex) {
+            System.out.println("error saving game");
+        }
+
+    }
+
+    public SudokuBoard loadGame(DifficultyLevel level) {
+        try {
+            return FileManager.loadBoard(level.getfolderName());
+        } catch (IOException ex) {
+            System.out.println("error loading game");
+            return null;
+        }
+    }
+
+    public void deleteGame(DifficultyLevel level) {
+        FileManager.delete(level.getfolderName());
+    }
+
+    public void saveCurrentGame(SudokuBoard board) {
+        try {
+            FileManager.saveBoard("current", board);
+        } catch (IOException ex) {
+            System.out.println("error saving game");
+        }
+    }
+
+    public SudokuBoard loadCurrentGame() {
+        try {
+            return FileManager.loadBoard("current");
+        } catch (IOException ex) {
+            System.out.println("error loading game");
+            return null;
+        }
+    }
+
+    public boolean hasCurrentGame() {
+        return FileManager.exists("current");
+    }
+}
