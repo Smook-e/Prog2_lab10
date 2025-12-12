b1ec4ffdfaa09ebf618b22459ce2d273daa6557f
sara-0011 <sarahabiiba@icloud.com>
2025-12-12 23:39:20 +0200

    commit to sudokuView overriding

--- src/View/SudokuView.java
+++ src/View/SudokuView.java
@@ -4,10 +4,49 @@
  */
 package View;
 
+import controller.Viewable;
+import exceptions.InvalidGame;
+import exceptions.NotFoundException;
+import exceptions.SolutionInvalidException;
+import java.io.IOException;
+import model.Catalog;
+import model.DifficultyLevel;
+import model.Game;
+
 /**
  *
  * @author HP
  */
-public class SudokuView {
+public class SudokuView implements Viewable {
+
+    @Override
+    public Catalog getCatalog() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Game getGame(DifficultyLevel level) throws NotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void driveGames(Game source) throws SolutionInvalidException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String verifyGame(Game game) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int[] solveGame(Game game) throws InvalidGame {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void logUserAction(String userAction) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
