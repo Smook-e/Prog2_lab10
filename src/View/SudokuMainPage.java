05c442c7ad5aac6cc7ab45d76801ca5c03039312
sara-0011 <sarahabiiba@icloud.com>
2025-12-21 23:34:35 +0200

    commit to changing message in mainPage

--- src/View/SudokuMainPage.java
+++ src/View/SudokuMainPage.java
@@ -189,7 +189,7 @@ public class SudokuMainPage extends javax.swing.JFrame {
     private void button2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button2ActionPerformed
        if(view.getCatalog().current)
            {
-               JOptionPane.showMessageDialog(this,"Couldn't find a previous game.");
+               JOptionPane.showMessageDialog(this,"finish current game first.");
                return;
            }
         newGame(DifficultyLevel.EASY);
@@ -198,7 +198,7 @@ public class SudokuMainPage extends javax.swing.JFrame {
     private void button3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button3ActionPerformed
       if(view.getCatalog().current)
            {
-               JOptionPane.showMessageDialog(this,"Couldn't find a previous game.");
+               JOptionPane.showMessageDialog(this,"finish current game first.");
                return;
            }
         newGame(DifficultyLevel.MEDIUM);
@@ -207,7 +207,7 @@ public class SudokuMainPage extends javax.swing.JFrame {
     private void button4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button4ActionPerformed
      if(view.getCatalog().current)
            {
-               JOptionPane.showMessageDialog(this,"Couldn't find a previous game.");
+               JOptionPane.showMessageDialog(this,"finish current game first.");
                return;
            }
         newGame(DifficultyLevel.HARD);
